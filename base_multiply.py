ac35f65ce38af5aaa609d8030576cadebb8c26a1
Edward Bassett <ebassett@ualberta.ca>
2015-05-16 19:07:43 -0600

    Add arg-parsing.
    Add arg-parsing:
    - positional x and y are required
    - --base is optional, with default of 10
    - --debug option prints debugging output

--- base_multiply.py
+++ base_multiply.py
@@ -2,17 +2,34 @@
 # -*- coding: utf-8 -*-
 
 # base_multiply - 2014-10-12 - ejb
+__version__ = '0.1'
+'''
+Revision history
+0.1 Initial version
+'''
 
+
+import argparse
 from base_addition import base_addition
 import sys
 
 
-DEBUG = True
 DIGITS = '0123456789ABCDEF'
 
 
+def parse_args():
+    parser = argparse.ArgumentParser(description="Multiply two numbers in a given base (default 10).")
+    parser.add_argument("input_x", type=str, metavar="x", help="first  multiplicand")
+    parser.add_argument("input_y", type=str, metavar="y", help="second multiplicand")
+    parser.add_argument("-b", "--base", type=int, dest="base", choices=xrange(2, 17), default=10, help="base (2-16); default = %(default)s")
+    parser.add_argument('--version', action='version', version='%(prog)s v' + str(__version__))
+    parser.add_argument("--debug", "--verbose", action="store_true", dest="debug", help="enable debugging output")
+    return parser.parse_args()
+
+
+
 def log_debug(debug_str):
-    if DEBUG:
+    if debug:
         print "DEBUG: ", debug_str
 
 def log_error(error_str):
@@ -22,12 +39,16 @@ def log_error(error_str):
 
 
 if __name__ == "__main__":
-    base = 16
+    args = parse_args()
+    debug = args.debug
+    log_debug(args)
+
+    base = args.base
     digits = DIGITS[0:base]
     log_debug('base: %s' % base)
 
-    input_x = '550101' #'777'
-    input_y = 'FA0' #'5501'
+    input_x = args.input_x
+    input_y = args.input_y
     log_debug('x: %s' % input_x)
     log_debug('y: %s' % input_y)
     x = input_x[::-1]  # Reverse string so we can do right-to-left processing left-to-right
